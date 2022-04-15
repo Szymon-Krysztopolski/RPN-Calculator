a58d3fe7a4af70bb0214a6d5364b4448a846f4ac
Szymon-Krysztopolski <94224331+Szymon-Krysztopolski@users.noreply.github.com>
2022-04-15 15:01:40 +0200

    prawie wszystkie funkcje pozostalo - undo, settings

--- app/src/main/java/com/example/rpn_calculator/Calculator.kt
+++ app/src/main/java/com/example/rpn_calculator/Calculator.kt
@@ -6,6 +6,8 @@ import android.widget.TextView
 import java.lang.Exception
 import java.util.*
 import kotlin.math.min
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 class Calculator(tmp: MainActivity) {
     private val ma = tmp
@@ -27,13 +29,27 @@ class Calculator(tmp: MainActivity) {
                 R.id.settings -> Toast.makeText(ma.applicationContext, "here", Toast.LENGTH_SHORT).show()
                 R.id.actChgSign -> chgSign()
                 R.id.enter -> stack_add()
-                else -> Toast.makeText(ma.applicationContext, button.text, Toast.LENGTH_SHORT).show()
+                R.id.back -> main_num_del()
+                R.id.actAdd -> action2arg('+')
+                R.id.actSub -> action2arg('-')
+                R.id.actMulti -> action2arg('*')
+                R.id.actDiv -> action2arg('/')
+                R.id.pow -> action2arg('p')
+                R.id.swap -> action2arg('s')
+                R.id.sqrt -> action1arg('q')
+                R.id.drop -> action1arg(' ')
+                R.id.ac -> stack_clear()
+                else -> MYtoast_mess(button.text.toString())
             }
         }
     }
 
     private fun main_num_add(s: String){
         main_text.text = main_text.text.toString().plus(s)
+    }
+
+    private fun main_num_del(){
+        main_text.text = main_text.text.dropLast(1);
     }
 
     private fun chgSign(){
@@ -51,17 +67,67 @@ class Calculator(tmp: MainActivity) {
             stack.push(main_text.text.toString().toDouble())
             stack_update()
         } catch (e: Exception){
-            Toast.makeText(ma.applicationContext, "Error with conversion to double", Toast.LENGTH_SHORT).show()
+            MYtoast_mess("Error with conversion to double")
         } finally {
             main_text.text=""
         }
     }
 
     private fun stack_update(){
+        for(i in 0 until stack_SIZE) screen_stack[i].text = "s".plus((i+1).toString().plus(": "))
+
         if(stack.isNotEmpty()){
             for(i in 0 until min(stack_SIZE,stack.size)){
                 screen_stack[i].text = "s".plus((i+1).toString().plus(": ".plus(stack[stack.size-i-1].toString())))
             }
         }
+    }
+
+    private fun stack_clear(){
+        stack.clear()
+        stack_update()
+    }
+
+    private fun action1arg(act: Char){
+        if(stack.isNotEmpty()){
+            val a: Double = stack.pop()
+            when(act){
+                'q' -> stack.push(sqrt(a))
+            }
+            stack_update()
+        } else {
+            MYtoast_mess("stack is empty")
+        }
+    }
+
+    private fun action2arg(act: Char){
+        if(stack.size>=2){
+            val a: Double = stack.pop()
+            val b: Double = stack.pop()
+            when(act) {
+                '+' -> stack.push(a + b)
+                '-' -> stack.push(a - b)
+                '*' -> stack.push(a * b)
+                '/' -> {
+                    if (b!=0.0) stack.push(a / b) else {
+                        stack.push(b)
+                        stack.push(a)
+                        MYtoast_mess("you can't div by zero")
+                    }
+                }
+                'p' -> stack.push(a.pow(b))
+                's' -> {
+                    stack.push(a)
+                    stack.push(b)
+                }
+            }
+            stack_update()
+        } else {
+            MYtoast_mess("size of stack is below 2")
+        }
+    }
+
+    private fun MYtoast_mess(mess: String){
+        Toast.makeText(ma.applicationContext, mess, Toast.LENGTH_SHORT).show()
     }
 }
